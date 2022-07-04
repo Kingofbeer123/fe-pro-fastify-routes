3ebf494ef7f21a0e59d752a597117981069644a5
unknown <C:\Users\hp\AppData\Roaming\The Bat!>
2022-07-04 12:45:30 +0300

    test

--- src/index.js
+++ src/index.js
@@ -14,22 +14,26 @@ fastify.register(import('@fastify/cookie'));
 
 fastify.post('/uppercase', (request, reply) => {
   let str = request.body.toUpperCase();
-  if (body.includes('FUCK')) {
+  if (str.includes('FUCK')) {
     return reply.status(403).send('unresolved');
   }
   else {
-    return reply.send(body);
+    return reply.status(200).send(str);
   }
 });
 
 fastify.post('/lowercase', (request, reply) => {
  let str = request.body.toLowerCase();
- if (body.includes('fuck')) {
+ if (str.includes('fuck')) {
     return reply.status(403).send('unresolved');
   }
   else {
-    return reply.send(body);
+    return reply.status(200).send(str);
   }
 });
+
+fastify.get('/user/:id', (request, reply) => {
+
+})
 
 export default fastify;
